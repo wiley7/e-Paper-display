9183548d7c062523b58f6db84efacc0c0893f12b
yewei <yewei@topjoy.com>
2022-11-17 17:39:42 +0800

    add convert img

--- RaspberryPi_JetsonNano/python/scripts/img.py
+++ RaspberryPi_JetsonNano/python/scripts/img.py
@@ -13,9 +13,8 @@ import time
 from PIL import Image,ImageDraw,ImageFont
 import traceback
 import argparse
-
+from pathlib import Path
 logging.basicConfig(level=logging.DEBUG)
-
 
 def output(img_path):
     try:
@@ -26,11 +25,13 @@ def output(img_path):
         epd.Clear(0xFF, 0)
         
         logging.info("read 4 Gray bmp file")
+
         img_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), img_path)
-        logging.info("start output file" + img_file)
-        Himage = Image.open(img_file)
-        newSize = (epd.height, epd.width)
-        Himage.resize(newSize)
+        filename = os.path.basename(img_file)
+        target_file = os.path.join(os.path.dirname(img_file), "target.".filename)
+        convert(mode="L2", source=img_file, target=target_file, width=epd.width, height=epd.height,dither=True, threshold=[63, 126, 189])
+        logging.info("start output file" + target_file)
+        Himage = Image.open(target_file)
         epd.display_4Gray(epd.getbuffer_4Gray(Himage))
         
         logging.info("Goto Sleep...")
@@ -44,6 +45,111 @@ def output(img_path):
         epd3in7.epdconfig.module_exit()
         exit()
 
+def thr(px, threshold):
+    if px < threshold[0]:
+        return 0
+    elif threshold[0] <= px < threshold[1]:
+        return 85
+    elif threshold[1] <= px < threshold[2]:
+        return 170
+    else:
+        return 255
+
+def convert(mode, source, target, width, height, dither, threshold):
+    data_bw = None
+    data_red = None
+
+    img = Image.open(source)
+
+    # Get rid of alpha and change it to white.
+    if img.mode == "RGBA":
+        tmp = Image.new("RGBA", img.size, "WHITE")
+        tmp.paste(img, mask=img)
+        img = tmp.convert("RGB")
+
+    # Scale image according to provided arguments.
+    if width is not None and height is not None:
+        img = img.resize((width, height))
+        print(f"Scaled to {width}x{height}")
+    elif width is not None:
+        w, h = img.size
+        ratio = h / w
+        img = img.resize((width, int(width * ratio)))
+        print(f"Scaled uniformly to: {width}x{int(width * ratio)}")
+    elif height is not None:
+        w, h = img.size
+        ratio = w / h
+        img = img.resize((int(height * ratio), height))
+        print(f"Scaled uniformly to: {int(height * ratio)}x{height}")
+    else:
+        pass
+
+    # Pad image to make width divisible by 8.
+    w, h = img.size
+    if w % 8 != 0:
+        w = w + 8 - w % 8
+        print(f"Image width not divisible by 8. Padded to width: {w}")
+        tmp = Image.new(img.mode, (w, h), (255, 255, 255))
+        tmp.paste(img)
+        img = tmp
+
+    # BW image conversion.
+    if mode == "L1":
+        if dither:
+            # Using "1" mode with dithering gives better results.
+            img = img.convert("1").point(lambda p: p > threshold[0] and 255)
+            # In this mode data match FrameBuffer HLSB format, no need for further processing.
+            data_bw = bytearray(img.tobytes())
+        else:
+            # Using "L" mode makes thresholding easier.
+            img = img.convert("L").point(lambda p: p > threshold[0] and 255)
+
+            # Convert pixel data to HLSB format.
+            img_data = img.tobytes()
+            data_bw = bytearray(w * h // 8)
+            for byte in range(w * h // 8):
+                pixels = img_data[byte * 8:(byte + 1) * 8]
+                tmp = 0
+                for i, v in enumerate(pixels):
+                    tmp |= (v & 0x1) << (7 - i)
+                data_bw[byte] = tmp
+
+    # 4 level grayscale conversion.
+    else:
+        # Converting to "P" mode first allows dithering.
+        img = img.convert("P", dither=Image.FLOYDSTEINBERG if dither else Image.NONE).convert("L").point(thr)
+
+        # Convert pixel data to bytearrays for BW and RED RAMs in HLSB format.
+        data_bw = bytearray(w * h // 8)
+        data_red = bytearray(w * h // 8)
+        img_data = img.tobytes()
+        for byte in range(w * h // 8):
+            pixels = img_data[byte * 8:(byte + 1) * 8]
+            tmp_bw = 0
+            tmp_red = 0
+            for i, v in enumerate(pixels):
+                tmp_bw |= (v & 0x1) << (7 - i)
+                tmp_red |= ((v >> 1) & 0x1) << (7 - i)
+            data_bw[byte] = tmp_bw
+            data_red[byte] = tmp_red
+
+    # Save file.
+    if target is None:
+        out_file = Path(source).stem + ".py"
+    else:
+        if target.split(".")[-1].lower() != ".py":
+            out_file = target + ".py"
+        else:
+            out_file = target
+    print(f"Saving data to: {out_file}")
+    with open(out_file, "w") as f:
+        f.write(f"width = {w}\n")
+        f.write(f"height = {h}\n")
+        f.write(f"img_bw = {data_bw}\n")
+        # Save data for RED RAM only if it exists.
+        if data_red:
+            f.write(f"img_red = {data_red}\n")
+
 if __name__ == '__main__':
     print('######################################')
 
@@ -54,5 +160,5 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--img_file', required=True)      # option that takes a value
     args = parser.parse_args()
     result = []
-    output(args.img_file)
+    output(args["img_file"])
     
